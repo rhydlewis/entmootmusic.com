fa6e9d186523c1347ac928aec34d7652393a1853
Rhyd Lewis <rhyd.lewis@icloud.com>
2024-11-14 07:08:32 +0000

    added bluesky

--- src/@lekoarts/gatsby-theme-emilia/components/social-media-list.tsx
+++ src/@lekoarts/gatsby-theme-emilia/components/social-media-list.tsx
@@ -29,13 +29,18 @@ const SocialMediaList = () => {
     <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
   </svg>
 </Themed.a>
-<Themed.a href="/contact" target="_blank" rel="noopener noreferrer" aria-label="Contact">
-  <svg fill="currentColor" viewBox="0 0 512 512" strokeLinecap="round" strokeLinejoin="round" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg">
+<Themed.a href="https://bsky.app/profile/musicbyentmoot.bsky.social" target="_blank" rel="noopener noreferrer" aria-label="BlueSky">
+  <svg fill="currentColor" viewBox="0 0 576 512" strokeLinecap="round" strokeLinejoin="round" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg">
+    <path d="M407.8 294.7c-3.3-.4-6.7-.8-10-1.3c3.4 .4 6.7 .9 10 1.3zM288 227.1C261.9 176.4 190.9 81.9 124.9 35.3C61.6-9.4 37.5-1.7 21.6 5.5C3.3 13.8 0 41.9 0 58.4S9.1 194 15 213.9c19.5 65.7 89.1 87.9 153.2 80.7c3.3-.5 6.6-.9 10-1.4c-3.3 .5-6.6 1-10 1.4C74.3 308.6-9.1 342.8 100.3 464.5C220.6 589.1 265.1 437.8 288 361.1c22.9 76.7 49.2 222.5 185.6 103.4c102.4-103.4 28.1-156-65.8-169.9c-3.3-.4-6.7-.8-10-1.3c3.4 .4 6.7 .9 10 1.3c64.1 7.1 133.6-15.1 153.2-80.7C566.9 194 576 75 576 58.4s-3.3-44.7-21.6-52.9c-15.8-7.1-40-14.9-103.2 29.8C385.1 81.9 314.1 176.4 288 227.1z"/>
+  </svg>
+</Themed.a>
+  <Themed.a href="/contact" target="_blank" rel="noopener noreferrer" aria-label="Contact">
+    <svg fill="currentColor" viewBox="0 0 512 512" strokeLinecap="round" strokeLinejoin="round" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg">
     <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
   </svg>
 </Themed.a>
 </>
-)
+  )
 }
 
 export default SocialMediaList
